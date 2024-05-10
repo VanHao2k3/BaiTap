2d448b90c1bf00b400b4a64cea542a5ba07dff21
VanHao2k3 <haocuaquakhu@gmail.com>
2024-05-10 11:08:36 +0700

    Buoi 2

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get("/",function(){ echo "Hello World!"; });
+Route::get("home", function(){ return "Home"; });
+Route::get("welcome", function(){return view("welcome");});
+Route::get("user",[UserController::class,"index"]);
+Route::get("user/1",[UserController::class,"show"]); 
+Route::get("user/{id}/edit",[UserController::class,"edit"])->name("user.edit");
+Route::get("user/list/{page?}",[UserController::class,"list"])->name("user.list");
